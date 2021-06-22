ca35ec5a467f84df947d05741783a2ae403ab843
David Pleydell <david.pleydell@inrae.fr>
2021-06-22 16:45:02 +0200

    Updated package name in demo.

--- nimbleAPT/demo/APT_demo.R
+++ nimbleAPT/demo/APT_demo.R
@@ -1,17 +1,17 @@
 ##################################################################################################################
 ## This script provides a rather contrived toy example designed to exhibit poor MCMC mixing with standard samplers
 ## Data is simulate with a simple "right" model.
-## Parameters are then estimated for a "wrong" model, which resembles a misspecified mixture model. 
+## Parameters are then estimated for a "wrong" model, which resembles a misspecified mixture model.
 ##################################################################################################################
 
 library(coda)
-library(NimbleSnippets)
+library(nimbleAPT)
 
 ## usePackage <- TRUE ## FALSE
 ## if (usePackage) {
 ##     ## THIS FAILS
-##     ## library(nimble)    
-##     ## library(NimbleSnippets)
+##     ## library(nimble)
+##     ## library(nimbleAPT)
 ##     print(search()) ## [1] ".GlobalEnv" "package:coda" "package:nimble" "ESSR"  "package:stats" "package:graphics" "package:grDevices" "package:utils" "package:datasets" "package:methods" "Autoloads" "package:base"
 ##     print(environment())
 ##     print(environment(buildMCMC)) ## <environment: 0x89a4f50>
@@ -19,7 +19,7 @@ library(NimbleSnippets)
 ## } else {
 ##     ## THIS WORKS
 ##     ## library(nimble)
-##     packDir <- find.package("NimbleSnippets")
+##     packDir <- find.package("nimbleAPT")
 ##     source(paste0(packDir, "/APT_functions.R"))
 ##     source(paste0(packDir, "/APT_samplers.R"))
 ##     source(paste0(packDir, "/APT_build.R"))
@@ -48,37 +48,37 @@ k2theta <- nimbleFunction(
         ## Uses a categorical rv to apply a shift in the value of a parameter theta
         k <- ceiling(k)
         if (k < 1 | k > K)
-            return(-Inf) 
+            return(-Inf)
         theta <- theta0 + randomEffects[k]
         return(theta)
         returnType(double())
     }
 )
-## k2theta(k=wrong$k, K=K, randomEffects=wrong$rfx_theta, theta0=wrong$theta0) 
-## ck2theta <- compileNimble(k2theta)                  
-## ck2theta(k=wrong$k, K=K, randomEffects=wrong$rfx_theta, theta0=wrong$theta0) 
+## k2theta(k=wrong$k, K=K, randomEffects=wrong$rfx_theta, theta0=wrong$theta0)
+## ck2theta <- compileNimble(k2theta)
+## ck2theta(k=wrong$k, K=K, randomEffects=wrong$rfx_theta, theta0=wrong$theta0)
 
 k2sigma <- nimbleFunction(
     run = function(k = double(), K = double(), randomEffects = double(1), sigma0=double()) {
         ## Uses a categorical rv to determine a change in a standard deviation parameter theta
         k <- ceiling(k)
         if (k < 1 | k > K)
-            return(-Inf) 
+            return(-Inf)
         sigma <- abs(sigma0 + randomEffects[k])
         return(sigma)
         returnType(double())
     }
 )
-## k2sigma(k=wrong$k, K=K, randomEffects=wrong$rfx_sigma, sigma0=wrong$sigma0) 
-## ck2sigma <- compileNimble(k2sigma)                  
-## ck2sigma(k=wrong$k, K=K, randomEffects=wrong$rfx_sigma, sigma0=wrong$sigma0) 
+## k2sigma(k=wrong$k, K=K, randomEffects=wrong$rfx_sigma, sigma0=wrong$sigma0)
+## ck2sigma <- compileNimble(k2sigma)
+## ck2sigma(k=wrong$k, K=K, randomEffects=wrong$rfx_sigma, sigma0=wrong$sigma0)
 
 ##################################
 ## BUGS code for the 'right' model
 ##################################
 rightCode <- nimbleCode({
     theta ~ dnorm(0, sd=1E6)
-    for (i in 1:N) 
+    for (i in 1:N)
         y[i] ~ dnorm(mean=theta, sd=sigma0)
 })
 ##################################
@@ -87,9 +87,9 @@ rightCode <- nimbleCode({
 wrongCode <- nimbleCode({
     k      ~ dcat(pk[1:K])
     theta0 ~ dnorm(0, sd=1E6)
-    theta <- k2theta(k, K, rfx_theta[1:K], theta0) 
+    theta <- k2theta(k, K, rfx_theta[1:K], theta0)
     sigma0 ~ dexp(1)
-    sigma <- abs(k2sigma(k, K, rfx_sigma[1:K], sigma0)) 
+    sigma <- abs(k2sigma(k, K, rfx_sigma[1:K], sigma0))
     for (i in 1:N)
         y[i] ~ dnorm(mean=theta, sd=sigma)
 })
@@ -107,10 +107,10 @@ rfx_theta <- rnorm(K)
 rfx_sigma <- rnorm(K)
 ##
 rConstants <- list(N=N, pk=pk, sigma0=sigma0)
-rInits     <- list(theta=theta0) 
+rInits     <- list(theta=theta0)
 ##
 wConstants <- list(N=N, K=K)
-wInits     <- list(theta0=theta0, sigma0=sigma0, rfx_theta=rfx_theta, rfx_sigma=rfx_sigma, k=1, pk=pk) 
+wInits     <- list(theta0=theta0, sigma0=sigma0, rfx_theta=rfx_theta, rfx_sigma=rfx_sigma, k=1, pk=pk)
 
 ###########################
 ## Initialise the models ##
@@ -143,8 +143,8 @@ wrong2$calculate() ## Total logProb of wrong2
 #########################
 ## Compile wrong model ##
 #########################
-cWrong  <- compileNimble(wrong) 
-cWrong2 <- compileNimble(wrong2) 
+cWrong  <- compileNimble(wrong)
+cWrong2 <- compileNimble(wrong2)
 
 ###################################
 ## Setup MCMC samplers for wrong ##
@@ -188,7 +188,7 @@ apt$getMonitors2()
 MCMC  <- buildMCMC(mcmc)
 cMCMC <- compileNimble(MCMC)
 ## undebug(buildAPT)
-APT   <- buildAPT(apt, Temps=1:7, monitorTmax=TRUE) 
+APT   <- buildAPT(apt, Temps=1:7, monitorTmax=TRUE)
 cAPT  <- compileNimble(APT)
 
 ###############
@@ -258,7 +258,7 @@ cWrong2$sigma0 <- init["sigma0"]
 cWrong2$simulate(cWrong2$getDependencies(c("k","theta0","sigma0"), includeData=FALSE, self=FALSE))
 
 ## Rebuild APT with longer temperature ladder
-APT  <- buildAPT(apt, Temps=1:11, monitorTmax=TRUE) 
+APT  <- buildAPT(apt, Temps=1:11, monitorTmax=TRUE)
 cAPT <- compileNimble(APT)
 
 print("#########################")
@@ -269,7 +269,7 @@ THIN       <- 1
 cAPT$thin  <- THIN
 cAPT$thin2 <- THIN
 TuneTemper <- c(1, 1)  ## default value is c(10,1)
-meanL      <- cWrong2$calculate() 
+meanL      <- cWrong2$calculate()
 meanL_previous <- -Inf
 ii             <- 0
 while(ii==0 | meanL > meanL_previous + 2) {
@@ -277,15 +277,15 @@ while(ii==0 | meanL > meanL_previous + 2) {
     meanL_previous <- meanL
     print(paste0("iteration nb.", ii, "within while loop. meanL = ", meanL_previous))
     ##############################################################################################
-    ## Short run 1 - don't increase the flexibility of the AMH samplers & adapt temperature ladder   
+    ## Short run 1 - don't increase the flexibility of the AMH samplers & adapt temperature ladder
     nIter <- 5E4
     cAPT$thinPrintTemps <- round(nIter / 10) ## Ensures temps are only printed 10 times
     syst2 <- system.time(cAPT$run(nIter,
                                   reset          = FALSE, ## Do not reset the adaptive MCMC, let adaptation continue as it is
-                                  adaptTemps     = TRUE,  ## Allows temperature ladder to adjust 
+                                  adaptTemps     = TRUE,  ## Allows temperature ladder to adjust
                                   resetTempering = FALSE, ## Keeps the adjustments modest so avoids volatile behaviour. Setting to TRUE can make the temperature ladder make vary large changes
                                   printTemps     = TRUE,  ## Prevents verbose printing of temperature ladder updates
-                                  progressBar    = FALSE, 
+                                  progressBar    = FALSE,
                                   tuneTemper1=TuneTemper[1], tuneTemper2=TuneTemper[2]))
     nimPrint("While loop: 2nd short run finished. sysT = ", syst2[3])
     samples   <- tail(as.matrix(cAPT$mvSamples), floor(nIter/THIN))
@@ -297,30 +297,30 @@ while(ii==0 | meanL > meanL_previous + 2) {
     par(mfrow=c(1,1))
     plot(samples[,"theta0"],samples[,"sigma0"], xlab="theta0", ylab="sigma0")
     ##################################################################################################
-    ## Short run 2 - reset the flexibility of the AMH samplers & prevent temperature ladder adaptation   
+    ## Short run 2 - reset the flexibility of the AMH samplers & prevent temperature ladder adaptation
     nIter <- 5E4; cAPT$thinPrintTemps <- nIter / 10
     syst1 <- system.time(cAPT$run(nIter,
                                   reset          = TRUE,  ## Resets the adaptive MCMC. Let's proposal distributions change direction if required.
                                   adaptTemps     = FALSE, ## Prevents temperature ladder adaptation (to avoid volatile behaviour when counter is reset)
-                                  resetTempering = TRUE,  ## Resets counter used in temperature ladder adaptation. i.e. counter will be at nIter when adaptation is turned on in step 1 
+                                  resetTempering = TRUE,  ## Resets counter used in temperature ladder adaptation. i.e. counter will be at nIter when adaptation is turned on in step 1
                                   printTemps     = FALSE, ## Will print once only
-                                  progressBar    = TRUE, 
+                                  progressBar    = TRUE,
                                   tuneTemper1=TuneTemper[1], tuneTemper2=TuneTemper[2]))
     ## Update meanL
     nimPrint("While loop: 1st short run finished. sysT = ", syst1[3])
     ## Update meanL
-    samples2 <- tail(as.matrix(cAPT$mvSamples2), floor(nIter/THIN)) ## LogProbs 
+    samples2 <- tail(as.matrix(cAPT$mvSamples2), floor(nIter/THIN)) ## LogProbs
     meanL    <- mean(rowSums(samples2))
     nimPrint("meanL = ", meanL)
-    ## Calculate & print ESS 
+    ## Calculate & print ESS
     samples   <- tail(as.matrix(cAPT$mvSamples), floor(nIter/THIN)) ## Sampled parameters T=1
     mc        <- as.mcmc(samples)
     ESS       <- sort(effectiveSize(mc))
-    (suggestedThinning <- round(nIter/min(ESS))) 
+    (suggestedThinning <- round(nIter/min(ESS)))
     nimPrint("ESS = ", ESS[1])
     ## Plot 3: trajectories
     dev.set()
-    plot(mc)    
+    plot(mc)
 }
 
 print("#####################################")
@@ -329,18 +329,18 @@ print("#####################################")
 nIter <- 1E4 * suggestedThinning
 cAPT$thin <- cAPT$thin2 <- suggestedThinning
 syst2 <- system.time(cAPT$run(nIter,
-                              reset          = FALSE, 
-                              adaptTemps     = FALSE, 
-                              resetTempering = FALSE, 
-                              printTemps     = FALSE, 
+                              reset          = FALSE,
+                              adaptTemps     = FALSE,
+                              resetTempering = FALSE,
+                              printTemps     = FALSE,
                               progressBar    = TRUE))
 nimPrint("While loop: 2nd short run finished. sysT = ", syst2[3])
-samples <- tail(as.matrix(cAPT$mvSamples), floor(nIter/suggestedThinning)) 
+samples <- tail(as.matrix(cAPT$mvSamples), floor(nIter/suggestedThinning))
 codaAPT <- as.mcmc(samples)
 
-## Plot 1: trajectories 
+## Plot 1: trajectories
 dev.set()
-plot(codaAPT)                  ## Trajectories display good mixing 
+plot(codaAPT)                  ## Trajectories display good mixing
 ## Plot 2: autocorrelation
 dev.set()
 autocorr.plot(codaAPT)
@@ -353,4 +353,3 @@ dev.set()
 plotTempTraj(cAPT)
 
 (effectiveSize(codaAPT)) ## Much healthier!
-
